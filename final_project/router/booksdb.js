3bd75bb47b6eba231378313ea2abc10ee6cc09cb
Ariel Duran <airmig@gmail.com>
2024-09-06 09:52:46 -0600

    implemente get books, get books by isbn, get books by title, get books by author, get books reviews by isbn, register a user

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"data": [{"user": "airmig", "review": "This is a great book"}, {"user": "maria", "review": "Do not buy book is a waste of money!"}]}, "isbn": '978-3-16-148410-0'},
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "isbn": '978-3-16-148410-1' },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "isbn": '978-3-16-148410-3' },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "isbn": '978-3-16-148410-4' },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "isbn": '978-3-16-148410-5' },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "isbn": '978-3-16-148410-6' },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "isbn": '978-3-16-148410-7' },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "isbn": '978-3-16-148410-8' },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "isbn": '978-3-16-148410-9' },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "isbn": '978-3-16-148410-2' }
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,94 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+function findBooksByCriteria(books, criteria) {
+  let result = [];
+  for (let key in books) {
+      let match = true;
+      for (let [keyCriteria, valueCriteria] of Object.entries(criteria)) {
+          if (books[key][keyCriteria].toLowerCase() !== valueCriteria.toLowerCase()) {
+              match = false;
+              break;
+          }
+      }
+      if (match) {
+          result.push(books[key]);
+      }
+  }
+  return result; // Return an array of books that match all criteria
+}
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log(req.body);
+  if (!username)
+    return res.status(400).json({message: "username is required" });
+  if (!password)
+    return res.status(400).json({message: "password is required"});
+
+  let found_user = users.find(user => user.username == username);
+  if (found_user)
+    return res.status(400).json({message: "username already taken"})
+  
+  users.push({"username": username, "password": password})
+
+  return res.status(201).json({message: "user account created"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json({books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let criteria = {'isbn': isbn}
+  let result = findBooksByCriteria(books, criteria);
+  if (result.length == 0)
+    return res.status(404).json({message: "ISBN not found."});
+  return res.status(200).json({result});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let criteria = {'author': author}
+  let result = findBooksByCriteria(books, criteria);
+  if (result.length == 0)
+    return res.status(404).json({message: "Author not found."});
+  return res.status(200).json({result});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  let criteria = {'title': title}
+  let result = findBooksByCriteria(books, criteria);
+  if (result.length == 0)
+    return res.status(404).json({message: "Title not found."});
+  return res.status(200).json({result});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  let isbn = req.params.isbn;
+  let criteria = {'isbn': isbn}
+  let result = findBooksByCriteria(books, criteria);
+  if (result.length == 0)
+    return res.status(404).json({message: "ISBN not found."});
+  const hasReviews = result[0]?.reviews?.data !== undefined;
+  if (!hasReviews)
+    return res.status(404).json({message: "Book does not have any reviews."});
+  let reviews = result[0].reviews.data;
+  return res.status(200).json({reviews});
+ });
 
 module.exports.general = public_users;
